05f4b5620fd86a4c72f306accc9e5e96e5374330
Dhruva Juloori <djuloori@hawk.iit.edu>
2017-11-14 17:13:29 -0600

    first commit

---
+++ src/finalproj/models/UserEntity.java
@@ -0,0 +1,79 @@
+package finalproj.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by Dhruva Juloori on 11/14/2017.
+ */
+@Entity
+@Table(name = "user", schema = "project_515")
+public class UserEntity {
+    private int iduser;
+    private String username;
+    private String password;
+    private String usertype;
+
+    @Id
+    @Column(name = "iduser")
+    public int getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(int iduser) {
+        this.iduser = iduser;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "usertype")
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (iduser != that.iduser) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (usertype != null ? !usertype.equals(that.usertype) : that.usertype != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = iduser;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (usertype != null ? usertype.hashCode() : 0);
+        return result;
+    }
+}
